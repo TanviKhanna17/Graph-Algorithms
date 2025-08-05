595fc6872938f8d974cf52a1d01a6ef4235fa4d2
TanviKhanna17 <76871243+TanviKhanna17@users.noreply.github.com>
2025-08-05 15:45:10 +0530

    Dijkstra's using set

--- Dijkstra's/DijkstraSet.cpp
+++ Dijkstra's/DijkstraSet.cpp
@@ -16,12 +16,16 @@ vector<int> Dijkstras(int n, vector<vector<int>> adj[])
     // Maintains all the shortest distances from the start position
     vector<int> dist(n, INT_MAX);
 
-    // Put Starting node inside the pq
+    // Put Starting node inside the set
     st.insert({ 0,0 });
     dist[0] = 0;
 
 
     // THE ACTUAL ALGORITHM:
+    // STEP 1 : EXTRACT SMALLEST DISTANCE FROM THE SET
+    // STEP 2 : FOR ALL ITS ADJACENT NODES, CHECK WHETHER THE NEW FOUND DIST IS BETTER THAN CURR DIST
+    // STEP 3 : IF IT IS BETTER , UPDATE THE DIST ARRAY, AND ADD THE NEW DIST BACK TO THE SET TO REACH OTHER NODES A BETTER WAY
+    // STEP 4 : KEEP UPDATING TILL ALL THE DIFFERENT DISTANCES HAVE BEEN CONVERED
 
     while (st.size() > 0)
     {
